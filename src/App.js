703c82bb27a9bf84f3b2e0a3ee56c6ad12eee3ca
Santos Gonzalez <sgonzo3@gmail.com>
2019-09-24 23:54:46 -0400

    feat: test 1 and 2 pass

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import marked from 'marked';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function showEditorHTML() {
+
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      value: "Enter content is the Text Area above."
+    }
+  }
+
+  editorChange = (e) => {
+    let content = e.target.value;
+    content = marked(content);
+    this.setState({value: content});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="title1">Markdown Previewer</h1>
+        </header>
+        <main>
+          <textarea
+            className="editor"
+            id="editor"
+            onKeyUp={this.editorChange}
+            placeholder="Compose your Markdown here..."
+            ></textarea>
+          <section className="preview" id="preview">
+            {this.state.value}
+          </section>
+        </main>
+        <footer>
+          <span>Made by <a href="https://sgonzo3.github.io">Santos Gonzalez</a></span>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
