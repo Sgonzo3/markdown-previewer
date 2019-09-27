469ff1e8297a7850fb6b403454fbbf0cde2cbfa1
Santos Gonzalez <sgonzo3@gmail.com>
2019-09-26 20:24:08 -0400

    Updates

---
+++ precache-manifest.44d3193f572c5295efdbe61059933c65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf00fc9af809fbf159639e39396030f",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d361b0b78cead2d38c74",
+    "url": "/my-app/static/css/main.2f1099a4.chunk.css"
+  },
+  {
+    "revision": "6868787faa7defb1c5cc",
+    "url": "/my-app/static/js/2.0189bc8d.chunk.js"
+  },
+  {
+    "revision": "d361b0b78cead2d38c74",
+    "url": "/my-app/static/js/main.6f70a97e.chunk.js"
+  },
+  {
+    "revision": "ed19d99f6b67da4ff3af",
+    "url": "/my-app/static/js/runtime-main.fffdd855.js"
+  }
+]);
